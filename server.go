cef48902f838b9c783e3b29968d3cee2eff8f554
DESKTOP-GDR14EG\andrewberumen <andrewberumen1991@gmail.com>
2025-12-01 11:20:42 -0800

    Add customerServer method

--- server.go
+++ server.go
@@ -1,4 +1,11 @@
 package main
 
-type CustomerServer struct {
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+)
+
+func CustomerServer(request *http.Request, writer *httptest.ResponseRecorder) {
+	fmt.Fprint(writer, "20000")
 }
