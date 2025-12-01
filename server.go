2fd704bbde9c60969c7f84b707960d114ab22b56
DESKTOP-GDR14EG\andrewberumen <andrewberumen1991@gmail.com>
2025-12-01 11:59:45 -0800

    Added code to get customer balance

--- server.go
+++ server.go
@@ -3,8 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
+type Customer struct {
+	Name    string
+	Balance string
+}
+
 func CustomerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20000")
+	customer := strings.TrimPrefix(r.URL.Path, "/customer/")
+	balance := GetCustomerBalance(customer)
+
+	fmt.Fprint(w, balance)
+}
+
+func GetCustomerBalance(c string) string {
+	if c == "Adam" {
+		return "20000"
+	}
+	if c == "Mary" {
+		return "10000"
+	}
+
+	return ""
 }
--- server_test.go
+++ server_test.go
@@ -7,15 +7,34 @@ import (
 )
 
 func TestGETCustomer(t *testing.T) {
-	request, _ := http.NewRequest(http.MethodGet, "/customer/Adam", nil)
-	response := httptest.NewRecorder()
 
-	CustomerServer(response, request)
+	t.Run("Should get a customer balance for Mary", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/customer/Mary", nil)
+		response := httptest.NewRecorder()
 
-	got := response.Body.String()
-	want := "20000"
+		CustomerServer(response, request)
 
+		got := response.Body.String()
+		want := "10000"
+		assertCustomerBalance(t, got, want)
+	})
+
+	t.Run("Should get a customer balance for Adam", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/customer/Adam", nil)
+		response := httptest.NewRecorder()
+
+		CustomerServer(response, request)
+
+		got := response.Body.String()
+		want := "20000"
+		assertCustomerBalance(t, got, want)
+	})
+}
+
+func assertCustomerBalance(t testing.TB, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("Got %q, Wanted %q", got, want)
 	}
+
 }
